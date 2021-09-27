23562fd40d5a7187ec275a94dfe4d749e055cfec
RichardL <richardl1010@outlook.com>
2021-09-27 19:55:07 -0300

    test

--- bot.js
+++ bot.js
@@ -1,87 +1,43 @@
 const { Client, Intents } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
-
-const video = require('ytdl-core');
 
 const { BOT_ID } = require('./config.json');
-const queue = new Map();
+
+const { musicPlayer } = require('./commands.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
-const player = createAudioPlayer();
-
+let musicHandler;
 client.on('ready', () => {
     console.log('Ready!!!');
+    musicHandler = new musicPlayer(client);
 })
 client.on('message', async (msg) => {
     if (msg.author.bot) return;
 
     let commands = msg.content.split(" ");
-
-    let channel = msg.member.voice.channel;
-
-    if (!channel) return msg.reply('você precisa estar em um caanal de voz')
-    if (msg.content === 'ola') {
+    if (msg.content === '!ola') {
         return msg.reply(`ola **${msg.author}`)
     }
-    else if (commands[0] === 'add') {
-        let songInfo = await video.getInfo(commands[1]);
-        let song = {
-            titulo: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url
-        }
-        const queueConstructor = {
-            textChannel: msg.channel,
-            VoiceChannel: channel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true,
-        }
-        await msg.reply(`${song.titulo} foi adcionada a fila`);
-        queue.set(msg.guild.id, queueConstructor);
-        queueConstructor.songs.push(song);
-        try {
+    else if (commands[0] === '!add') {
+        musicHandler.musicPlay(msg, commands[1])
 
-            queueConstructor.connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator
-            })
-            play(msg.guild, queueConstructor.songs[0])
-        } catch (error) {
-            console.log(error)
-            queue.delete(msg.guild.id)
-            return await msg.reply(error)
-        }
     }
-    else if (msg.content === 'pause') {
+    else if (msg.content === '!pause') {
         player.pause()
     }
-    else if (msg.content === 'play') {
+    else if (msg.content === '!play') {
         player.unpause()
     }
+    else if (msg.content === "!test") {
+        try{
+
+            musicHandler.addSong(msg, commands[1])
+        }catch(error){
+            console.log(error)
+        }
+    }
 })
-function play(server, music) {
-    let serverQueue = queue.get(server.id)
-    if (!music) {
-        serverQueue.VoiceChannel.leave()
-        queue.delete(server.id)
-        return true
-    }
-    let resource = createAudioResource(video(music.url), { inlineVolume: true })
-    resource.volume.setVolume(0.2)
-    serverQueue.connection.subscribe(player)
-    player.play(resource)
-    player.on((AudioPlayerStatus.Idle, () => {
-        player.play(getNext()
-        )
-    }
-    ))
-}
-function getNext(){
-    return queue[0]
-}
+
 try {
     client.login(BOT_ID)
 } catch (error) {
--- commands.js
+++ commands.js
@@ -1,1 +1,118 @@
-return true
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+
+const ytdl = require('ytdl-core');
+
+class musicPlayer {
+
+    constructor(client) {
+        this.client = client;
+
+        this.queue = new Map();
+
+        this.player = createAudioPlayer();
+
+    }
+
+    async addSong(msg,musicLink) {
+        let queue = this.queue.get(msg.guild.id)
+        if (queue) {
+            let queueConstructor = {
+                textChannel: msg.channel,
+                VoiceChannel: msg.member.voice.channel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true,
+            }
+            let songInfo = await ytdl.getInfo(musicLink);
+
+            let song = {
+                titulo: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url
+            }
+
+            await msg.reply(`${song.titulo} foi adcionada a fila`);
+
+            queueConstructor.songs.push(song);
+            this.queue.set(msg.guild.id, queueConstructor);
+            queue = this.queue.get(msg.guild.id)
+        }
+        else{
+            console.log('Done')
+        }
+    }
+    async musicPlay(msg, musicLink) {
+        let channel = msg.member.voice.channel;
+        if (!channel) return msg.reply('você precisa estar em um canal de voz');
+
+        this.queueConstructor = {
+            textChannel: msg.channel,
+            VoiceChannel: channel,
+            connection: null,
+            songs: [],
+            volume: 5,
+            playing: true,
+        }
+
+        let songInfo = await ytdl.getInfo(musicLink);
+
+        let song = {
+            titulo: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url
+        }
+
+        await msg.reply(`${song.titulo} foi adcionada a fila`);
+
+        this.queue.set(msg.guild.id, this.queueConstructor);
+        this.queueConstructor.songs.push(song);
+
+        try {
+
+            this.queueConstructor.connection = joinVoiceChannel({
+
+                channelId: channel.id,
+
+                guildId: channel.guild.id,
+
+                adapterCreator: channel.guild.voiceAdapterCreator
+            })
+
+            this.play(msg.guild, this.queueConstructor)
+
+            return true;
+
+        } catch (error) {
+            console.log(error)
+            this.queue.delete(msg.guild.id)
+            return await msg.reply(`Um erro ocorreu ${songInfo.titulo} foi removido\n${error}`)
+        }
+    }
+    play(server, queue) {
+        let music = queue.songs[0]
+        let serverQueue = this.queue.get(server.id)
+
+
+        if (!music) {
+
+            serverQueue.VoiceChannel.leave()
+
+            this.queue.delete(server.id)
+
+            return
+        }
+        let resource = createAudioResource(ytdl(music.url), { inlineVolume: true })
+
+        resource.volume.setVolume(0.2)
+
+        serverQueue.connection.subscribe(this.player)
+
+        this.player.play(resource)
+
+    }
+    getNext(server) {
+        console.log("getNext!!")
+    }
+
+}
+
+module.exports = { musicPlayer }
